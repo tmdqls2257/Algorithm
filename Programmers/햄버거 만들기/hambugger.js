37e57f110c2390ca84498ce366f9b9263fbc0bd7
tmdqls2257 <tmdqls2257@gmail.com>
2022-11-26 20:08:25 +0900

    햄버거 만들기

---
+++ Programmers/햄버거 만들기/hambugger.js
@@ -0,0 +1,21 @@
+function solution(ingredient) {
+  const stk = [];
+  let cnt = 0;
+
+  ingredient.forEach((ing, idx) => {
+    stk.push(ing);
+
+    if (stk.length >= 4) {
+      const find = stk.slice(-4).join("");
+      if (find === "1231") {
+        stk.pop();
+        stk.pop();
+        stk.pop();
+        stk.pop();
+        cnt++;
+      }
+    }
+  });
+
+  return cnt;
+}
